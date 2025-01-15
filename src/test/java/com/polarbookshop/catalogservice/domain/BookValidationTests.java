5ebb18cdc46bd45f0d69ed40ccb4e92c4aa45e92
Louis DBZ <louis.debouzy7@gmail.com>
2025-01-15 17:51:31 +0100

    Update BookValidationTests.java

--- src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
@@ -28,7 +28,7 @@ class BookValidationTests {
         var book = new Book("a234567891","Title","Author",9.90);
         Set <ConstraintViolation<Book>> violations = validator.validate(book);
         assertThat(violations).hasSize(1);
-        assertThat(violations.iterator().next().getMessage()).isEqualTo("The book isbn must be defined.");
+        assertThat(violations.iterator().next().getMessage()).isEqualTo("The ISBN format must be valid");
     }
 
 
