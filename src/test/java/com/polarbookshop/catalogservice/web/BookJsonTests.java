58c8c6f9af380062d47a77c65823867cfd2ea1da
Louis DBZ <louis.debouzy7@gmail.com>
2025-01-17 12:41:59 +0100

    updating catalog-service chap 4 end

--- src/test/java/com/polarbookshop/catalogservice/web/BookJsonTests.java
+++ src/test/java/com/polarbookshop/catalogservice/web/BookJsonTests.java
@@ -1,5 +1,9 @@
 package com.polarbookshop.catalogservice.web;
 
+//import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import	static org.assertj.core.api.Assertions.assertThat;
@@ -13,17 +17,31 @@ class BookJsonTests {
     @Autowired
     private JacksonTester<Book> json;
 
+
+    //@Disabled
+
+    ///disabling this test momentanelly, annotation @Disabled doesn't work
+
     /***
-    @Test
+     @Test
     void testserialize() throws Exception {
         var book = new Book("1234567890","title","author",9.90);
         var jsonContent = json.write(book);
+
+        String jsonString = jsonContent.getJson();
+        // Convert the JSON string to a JsonNode using ObjectMapper
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(jsonString);
+
+        //JsonNode jsonNode = jsonContent.getJsonNode();
+        //String jsonString = jsonContent.getJson();
         assertThat(jsonContent).extractingJsonPathStringValue("@.isbn").isEqualTo(book.isbn());
         assertThat(jsonContent).extractingJsonPathStringValue("@.title").isEqualTo(book.title());
         assertThat(jsonContent).extractingJsonPathStringValue("@.author").isEqualTo(book.author());
         assertThat(jsonContent).extractingJsonPathStringValue("@.price").isEqualTo(book.price());
     }
-    ***/
+
+     ***/
 
     @Test
     void testDeserialize() throws Exception {
